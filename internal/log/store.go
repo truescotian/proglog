4cd2e85f89ffdff10e2411be486e85172018f3ed
truescotian <gregpmillr@gmail.com>
2021-09-05 11:33:37 -0300

    completed page 29

---
+++ internal/log/store.go
@@ -0,0 +1,99 @@
+package log
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenWidth = 8
+)
+
+type store struct {
+	*os.File
+	mu   sync.Mutex
+	buf  *bufio.Writer
+	size uint64
+}
+
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}
+}
+
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+	w += lenWidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
+
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	// flush in case we're about to try to read a record
+	// that the buffer hasn't flushed to disk yet.
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+
+	// find out how many bytes we have to read to get the whole
+	// record.
+	size := make([]byte, lenWidth)
+	// reads len(size) bytes into size from the file starting at pos.
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+
+	// fetch the record
+	b := make([]byte, enc.Uint64(size))
+	// reads len(b) bytes into b from the file starting at pos+lenWidth
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+
+	// reads len(p) bytes into p from the file starting at off
+	return s.File.ReadAt(p, off)
+}
+
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	err := s.buf.Flush()
+	if err != nil {
+		return err
+	}
+	return s.File.Close()
+}
---
+++ internal/log/store_test.go
@@ -0,0 +1,29 @@
+package log
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+var (
+	write = []byte("hello world")
+	width = uint64(len(write)) + lenWidth
+)
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_append_read_test")
+	require.NoErrr(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	s, err = newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+}
