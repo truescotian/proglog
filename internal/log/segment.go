a333847b7db6771dd9800c3d8a2ac27eff1ec2e7
truescotian <gregpmillr@gmail.com>
2021-10-07 14:21:41 -0300

    On the bottom of page 42. Time to code the log

--- internal/log/segment.go
+++ internal/log/segment.go
@@ -1,8 +1,143 @@
 package log
 
+import (
+	"fmt"
+	"os"
+	"path"
+
+	api "github.com/truescotian/proglog/api/v1"
+	"google.golang.org/protobuf/proto"
+)
+
 type segment struct {
 	store                  *store
 	index                  *index
 	baseOffset, nextOffset uint64
 	config                 Config
+}
+
+func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
+	s := &segment{
+		baseOffset: baseOffset,
+		config:     c,
+	}
+	var err error
+	storeFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".store")),
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.store, err = newStore(storeFile); err != nil {
+		return nil, err
+	}
+	indexFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".index")),
+		os.O_RDWR|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.index, err = newIndex(indexFile, c); err != nil {
+		return nil, err
+	}
+	if off, _, err := s.index.Read(-1); err != nil {
+		s.nextOffset = baseOffset
+	} else {
+		s.nextOffset = baseOffset + uint64(off) + 1
+	}
+	return s, nil
+}
+
+func (s *segment) Append(record *api.Record) (offset uint64, err error) {
+	cur := s.nextOffset
+	record.Offset = cur
+	p, err := proto.Marshal(record)
+	if err != nil {
+		return 0, err
+	}
+	_, pos, err := s.store.Append(p)
+	if err != nil {
+		return 0, err
+	}
+	if err = s.index.Write(
+		// since index offsets are relative to the base offset, we
+		// subtract the segment's next offset from its base offset (which
+		// are both absolute offsets) to get the entry's relative offset
+		// in the segment.
+		uint32(s.nextOffset-uint64(s.baseOffset)),
+		pos,
+	); err != nil {
+		return 0, err
+	}
+	s.nextOffset++
+	return cur, nil
+}
+
+// Read returns the record for the given offset. Similar to writes, to read
+// a record the segment must first translate the absolute index into
+// a relative offset and get the associated index entry.
+func (s *segment) Read(off uint64) (*api.Record, error) {
+	// get the index entry
+	_, pos, err := s.index.Read(int64(off - s.baseOffset))
+	if err != nil {
+		return nil, err
+	}
+
+	// now that we have the index entry, the segment can go straight to the
+	// record's position in the store and read the proper amount of data.
+	bytes, err := s.store.Read(pos)
+	if err != nil {
+		return nil, err
+	}
+	record := &api.Record{}
+	err = proto.Unmarshal(bytes, record)
+	return record, err
+}
+
+// IsMaxed returns whether the segment has reached its max size, either by
+// writing too much to the store or the index. If you wrote a small number
+// of long logs, then you'd hit the segment store bytes limit; if you wrote
+// a lot of small logs, then you'd hit the index bytes limit. The log uses
+// this method to know it needs to create a new segment.
+func (s *segment) IsMaxed() bool {
+	return s.store.size >= s.config.Segment.MaxStoreBytes ||
+		s.index.size >= s.config.Segment.MaxIndexBytes
+}
+
+// Remove closes the segment and removes the index and store files.
+func (s *segment) Remove() error {
+	if err := s.Close(); err != nil {
+		return err
+	}
+	if err := os.Remove(s.index.Name()); err != nil {
+		return err
+	}
+	if err := os.Remove(s.store.Name()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *segment) Close() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// nearestMultiple returns the nearest and lesser multiple of k in j,
+// for example nearestMultiple(9,4) == 8. We take the lesser multiple
+// to make sure we stay under the user's disk capacity.
+func nearestMultiple(j, k uint64) uint64 {
+	if j >= 0 {
+		return (j / k) * k
+	}
+	return ((j - k + 1) / k) * k
 }
